c6402b7634072694a88ea653411fdf96f5eaf39c
Pedro Santos <47487770+p-pedroSantos@users.noreply.github.com>
2026-05-08 00:22:50 +0000

    teste pipeline falhar

--- etl.py
+++ etl.py
@@ -1,6 +1,6 @@
 def limpar_nome(nome):
     # Simula uma limpeza: remove espaços e coloca em maiúsculas
-    return nome.strip().upper()
+    return nome.strip()
 
 if __name__ == "__main__":
     print(limpar_nome("  dataflow etl  "))
